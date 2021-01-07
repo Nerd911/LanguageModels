940dd0c4831770fe08913319f966e537706647c6
Mikołaj Słupiński <mikolajslupinski@gmail.com>
2021-01-07 17:11:54 +0100

    Added script for model trining

---
+++ scripts/train_models.sh
@@ -0,0 +1,14 @@
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_W.txt > models/kenlm_o2_W.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_B.txt > models/kenlm_o2_B.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_T.txt > models/kenlm_o2_T.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_S2.txt > models/kenlm_o2_S2.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_S3.txt > models/kenlm_o2_S3.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_S4.txt > models/kenlm_o2_S4.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_S5.txt > models/kenlm_o2_S5.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_P2.txt > models/kenlm_o2_P2.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_P3.txt > models/kenlm_o2_P3.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_P4.txt > models/kenlm_o2_P4.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_P5.txt > models/kenlm_o2_P5.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_PW.txt > models/kenlm_o2_PW.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_SW.txt > models/kenlm_o2_SW.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_PSW.txt > models/kenlm_o2_PSW.arpa
