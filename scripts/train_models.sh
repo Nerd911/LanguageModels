d2a0ab3b13904b8133357c1653b19731b05ca155
Mikołaj Słupiński <mikolajslupinski@gmail.com>
2021-01-07 22:07:42 +0100

    Updated model training script

--- scripts/train_models.sh
+++ scripts/train_models.sh
@@ -1,14 +1,41 @@
-../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_W.txt > models/kenlm_o2_W.arpa
-../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_B.txt > models/kenlm_o2_B.arpa
-../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_T.txt > models/kenlm_o2_T.arpa
-../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_S2.txt > models/kenlm_o2_S2.arpa
-../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_S3.txt > models/kenlm_o2_S3.arpa
-../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_S4.txt > models/kenlm_o2_S4.arpa
-../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_S5.txt > models/kenlm_o2_S5.arpa
-../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_P2.txt > models/kenlm_o2_P2.arpa
-../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_P3.txt > models/kenlm_o2_P3.arpa
-../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_P4.txt > models/kenlm_o2_P4.arpa
-../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_P5.txt > models/kenlm_o2_P5.arpa
-../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_PW.txt > models/kenlm_o2_PW.arpa
-../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_SW.txt > models/kenlm_o2_SW.arpa
-../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_PSW.txt > models/kenlm_o2_PSW.arpa
+# ../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_W.txt > models/kenlm_o2_W.arpa
+# ../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_B.txt > models/kenlm_o2_B.arpa
+# ../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_T.txt > models/kenlm_o2_T.arpa
+# ../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_S2.txt > models/kenlm_o2_S2.arpa
+# ../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_S3.txt > models/kenlm_o2_S3.arpa
+# ../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_S4.txt > models/kenlm_o2_S4.arpa
+# ../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_S5.txt > models/kenlm_o2_S5.arpa
+# ../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_P2.txt > models/kenlm_o2_P2.arpa
+# ../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_P3.txt > models/kenlm_o2_P3.arpa
+# ../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_P4.txt > models/kenlm_o2_P4.arpa
+# ../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_P5.txt > models/kenlm_o2_P5.arpa
+# ../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_PW.txt > models/kenlm_o2_PW.arpa
+# ../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_SW.txt > models/kenlm_o2_SW.arpa
+# ../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_PSW.txt > models/kenlm_o2_PSW.arpa
+../kenlm/bin/lmplz -o 3 < data/task3_train_segmented_W.txt > models/kenlm_o2_W.arpa
+../kenlm/bin/lmplz -o 3 < data/task3_train_segmented_B.txt > models/kenlm_o2_B.arpa
+../kenlm/bin/lmplz -o 3 < data/task3_train_segmented_T.txt > models/kenlm_o2_T.arpa
+../kenlm/bin/lmplz -o 3 < data/task3_train_segmented_S2.txt > models/kenlm_o2_S2.arpa
+../kenlm/bin/lmplz -o 3 < data/task3_train_segmented_S3.txt > models/kenlm_o2_S3.arpa
+../kenlm/bin/lmplz -o 3 < data/task3_train_segmented_S4.txt > models/kenlm_o2_S4.arpa
+../kenlm/bin/lmplz -o 3 < data/task3_train_segmented_S5.txt > models/kenlm_o2_S5.arpa
+../kenlm/bin/lmplz -o 3 < data/task3_train_segmented_P2.txt > models/kenlm_o2_P2.arpa
+../kenlm/bin/lmplz -o 3 < data/task3_train_segmented_P3.txt > models/kenlm_o2_P3.arpa
+../kenlm/bin/lmplz -o 3 < data/task3_train_segmented_P4.txt > models/kenlm_o2_P4.arpa
+../kenlm/bin/lmplz -o 3 < data/task3_train_segmented_P5.txt > models/kenlm_o2_P5.arpa
+../kenlm/bin/lmplz -o 3 < data/task3_train_segmented_PW.txt > models/kenlm_o2_PW.arpa
+../kenlm/bin/lmplz -o 3 < data/task3_train_segmented_SW.txt > models/kenlm_o2_SW.arpa
+../kenlm/bin/lmplz -o 3 < data/task3_train_segmented_PSW.txt > models/kenlm_o2_W_PSW.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_B_W.txt > models/kenlm_o2_W_B.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_T_W.txt > models/kenlm_o2_W_T.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_S2_W.txt > models/kenlm_o2_W_S2.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_S3_W.txt > models/kenlm_o2_W_S3.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_S4_W.txt > models/kenlm_o2_W_S4.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_S5_W.txt > models/kenlm_o2_W_S5.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_P2_W.txt > models/kenlm_o2_W_P2.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_P3_W.txt > models/kenlm_o2_W_P3.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_P4_W.txt > models/kenlm_o2_W_P4.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_P5_W.txt > models/kenlm_o2_W_P5.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_PW_W.txt > models/kenlm_o2_W_PW.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_SW_W.txt > models/kenlm_o2_W_SW.arpa
+../kenlm/bin/lmplz -o 2 < data/task3_train_segmented_PSW_W.txt > models/kenlm_o2_W_PSW.arpa
