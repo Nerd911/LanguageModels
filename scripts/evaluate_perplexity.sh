454976cb4a0c264ddc02ea1a88b01c74c87924cd
Mikołaj Słupiński <mikolajslupinski@gmail.com>
2021-01-17 15:44:25 +0100

    Added evaluation script

---
+++ scripts/evaluate_perplexity.sh
@@ -0,0 +1,14 @@
+../../srilm/lm/bin/i686-m64/ngram -lm kenlm_o2_B.arpa -ppl ../data/task3_train_segmented_B.txt -order 2 -unk >> perplexity.log
+../../srilm/lm/bin/i686-m64/ngram -lm kenlm_o2_W.arpa -ppl ../data/task3_train_segmented_W.txt -order 2 -unk >> perplexity.log
+../../srilm/lm/bin/i686-m64/ngram -lm kenlm_o2_T.arpa -ppl ../data/task3_train_segmented_T.txt -order 2 -unk >> perplexity.log
+../../srilm/lm/bin/i686-m64/ngram -lm kenlm_o2_S2.arpa -ppl ../data/task3_train_segmented_S2.txt -order 2 -unk >> perplexity.log
+../../srilm/lm/bin/i686-m64/ngram -lm kenlm_o2_S3.arpa -ppl ../data/task3_train_segmented_S3.txt -order 2 -unk >> perplexity.log
+../../srilm/lm/bin/i686-m64/ngram -lm kenlm_o2_S4.arpa -ppl ../data/task3_train_segmented_S4.txt -order 2 -unk >> perplexity.log
+../../srilm/lm/bin/i686-m64/ngram -lm kenlm_o2_S5.arpa -ppl ../data/task3_train_segmented_S5.txt -order 2 -unk >> perplexity.log
+../../srilm/lm/bin/i686-m64/ngram -lm kenlm_o2_P2.arpa -ppl ../data/task3_train_segmented_P2.txt -order 2 -unk >> perplexity.log
+../../srilm/lm/bin/i686-m64/ngram -lm kenlm_o2_P3.arpa -ppl ../data/task3_train_segmented_P3.txt -order 2 -unk >> perplexity.log
+../../srilm/lm/bin/i686-m64/ngram -lm kenlm_o2_P4.arpa -ppl ../data/task3_train_segmented_P4.txt -order 2 -unk >> perplexity.log
+../../srilm/lm/bin/i686-m64/ngram -lm kenlm_o2_P5.arpa -ppl ../data/task3_train_segmented_P5.txt -order 2 -unk >> perplexity.log
+../../srilm/lm/bin/i686-m64/ngram -lm kenlm_o2_PW.arpa -ppl ../data/task3_train_segmented_PW.txt -order 2 -unk >> perplexity.log
+../../srilm/lm/bin/i686-m64/ngram -lm kenlm_o2_SW.arpa -ppl ../data/task3_train_segmented_SW.txt -order 2 -unk >> perplexity.log
+../../srilm/lm/bin/i686-m64/ngram -lm kenlm_o2_PSW.arpa -ppl ../data/task3_train_segmented_PS`W.txt -order 2 -unk >> perplexity.log
